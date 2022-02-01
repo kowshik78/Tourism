bd0248f5e26df4e8acab1e24c2986f16fda10d64
kowshik78 <kowshiksaha78@gmail.com>
2022-02-02 01:24:35 +0600

    Add files via upload

---
+++ common/headerTransparentLoggedOut.php
@@ -0,0 +1,26 @@
+<div class="col-sm-12">
+					
+	<div class="header">
+	
+		<div class="col-sm-4">
+	
+		<div class="logo"></div>
+		
+	</div>
+	
+		<div class="col-sm-8">
+	
+		<div class="headerMenu">
+			
+			<ul>
+				
+				<a href="login.php"><li>Login</li></a>
+				
+			</ul>
+			
+		</div>
+	</div>
+	
+	</div> <!-- header -->
+				
+</div> <!-- col-sm-12 MENU -->
